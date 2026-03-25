400cefc1a5cf65a291cfc143564ab5c1f9032cbc
Arun-Sioniq <designer@sioniqerp.com>
2026-03-25 12:23:20 +0530

    updated

--- server/ResumeBuilder.Api/Controllers/WeatherForecastController.cs
+++
@@ -1,1 +0,0 @@
-// This file is intentionally left empty - default template removed
--- server/ResumeBuilder.Api/Models/AdminUser.cs
+++
@@ -1,9 +0,0 @@
-namespace ResumeBuilder.Api.Models;
-
-public class AdminUser
-{
-    public int Id { get; set; }
-    public string Email { get; set; } = string.Empty;
-    public string PasswordHash { get; set; } = string.Empty;
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-}
